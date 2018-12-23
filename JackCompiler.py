00727ec39f5f1e8c4c3fe6d03159d97ba2df3a00
Azu <cxztpcom@gmail.com>
2018-12-23 23:22:14 +0800

    Roughly construct the structure.

--- JackCompiler.py
+++ JackCompiler.py
@@ -1,3 +1,57 @@
 
+from VMwriter import VMWriter
+
 class JackCompiler(object):
-    pass
+    """
+    Compile a jack class from token codes.
+    """
+
+    def __init__(self, parsed_codes, class_name):
+
+        self.parsed_codes = parsed_codes
+        self.progress = 0
+        self.class_name = class_name
+        self.writer = VMWriter(class_name + '.vm')
+
+    def write_class(self):
+        pass
+
+    def write_subroutine_dec(self):
+        pass
+
+    def write_subroutine_body(self):
+        pass
+
+    def write_parameter_list(self):
+        pass
+
+    def write_statements(self):
+        pass
+
+    def write_do(self):
+        pass
+
+    def write_return(self):
+        pass
+
+    def write_while(self):
+        pass
+
+    def write_if(self):
+        pass
+
+    def write_let(self):
+        pass
+
+    def write_expression(self):
+
+
+    def write_term(self):
+        pass
+
+    def write_expression_list(self):
+        pass
+
+    def _advance(self):
+
+
